a7fdbda392c566048d7d7a879913dc608fbc189b
Rounak Bhowmick <rounakchakda@gmail.com>
2020-07-05 13:58:56 +0530

    Completed

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,26 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+	public static void main(String[] args)
+			throws NumberFormatException, IOException, ClassNotFoundException, SQLException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		System.out.println("Enter skill Id");
+//		long skillId = Long.parseLong(br.readLine());
+//		System.out.println("Enter skill name");
+//		String skillName = br.readLine();
+//		Skill skill = new Skill(skillId, skillName);
+		SkillDAO skilldao = new SkillDAO();
+//		skilldao.insertdetails(skill);
+		List<Skill> list = skilldao.listAllSkills();
+		for (Skill a : list) {
+			System.out.println(a.getSkillId() + "\t" + a.getSkillName());
+		}
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,36 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+	List<Skill> skillList = new ArrayList<Skill>();
+
+	public List<Skill> listAllSkills() throws ClassNotFoundException, SQLException {
+		String sql = "select * from SKILLS ORDER BY SKILLNAME ASC";
+		PreparedStatement ps = ConnectionManager.getConnection().prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			long id = rs.getLong(1);
+			String name = rs.getString(2);
+			Skill skill = new Skill(id, name);
+			skillList.add(skill);
+		}
+		return skillList;
+	}
+
+	public void insertdetails(Skill skill) throws ClassNotFoundException, SQLException {
+		PreparedStatement ps = ConnectionManager.getConnection()
+				.prepareStatement("INSERT INTO SKILLs(ID, SKILLNAME) VALUES (?,?)");
+		ps.setLong(1, skill.getSkillId());
+		ps.setString(2, skill.getSkillName());
+		ps.executeUpdate();
+		System.out.println("Successfully Inseted!!!");
+	}
+}
